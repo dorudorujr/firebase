47ea64d7ecfa1993dc90c4746ec1da8357b6e414
杉岡成哉 <sugiokaseiya@team-lab.com>
2021-01-23 12:28:24 +0900

    HomeScreenが表示されるよに修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:moor_sample/screen/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(''),
-      ),
-    );
-  }
+  runApp(
+    ProviderScope(
+      child: HomeScreen(),
+    ),
+  );
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,222 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+import 'package:moor_sample/provider/provider.dart';
+import 'package:moor_sample/model/model.dart';
+import 'package:moor_sample/state_notifier/state_notifier.dart';
+import 'package:moor_sample/widget/task_tile.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var _newTaskTitle = '';
+    final _textEditingController = TextEditingController();
+
+    /// テキスト入力完了かTextFieldの×を押したら入力中の文字を消す
+    void clearTextField() {
+      _textEditingController.clear();
+      _newTaskTitle = '';
+    }
+
+    /// タスクを消したらSnackBar表示、restoreで元に戻せる
+    void showSnackBar({
+      List<Task> previousTasks,
+      TaskList taskList,
+      String content,
+      ScaffoldState scaffoldState,
+    }) {
+      /// SnackBar表示中にタスク削除したら、前のSnackBarを消すためにremoveを最初に入れている
+      /// ScaffoldState: snackBar管理クラス？
+      scaffoldState.removeCurrentSnackBar();
+      final snackBar = SnackBar(
+        content: Text(content),
+        action: SnackBarAction(
+          label: 'restore',
+          onPressed: () {
+            /// 消す前のタスクリストで更新して削除したタスクを復活させる
+            taskList.updateTasks(previousTasks);
+            scaffoldState.removeCurrentSnackBar();
+          },
+        ),
+        duration: const Duration(seconds: 3),
+      );
+      scaffoldState.showSnackBar(snackBar);
+    }
+
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Consumer(
+          builder: (context, watch, child) {
+            final taskList = watch(taskListProvider);
+            final allTasks = watch(taskListProvider.state);
+            final displayedTasks = watch(filteredTasks);
+            final filter = watch(filterProvider);
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Text(
+                            'ToDo List',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16,
+                          ),
+                          child: TextField(
+                            controller: _textEditingController,
+                            decoration: InputDecoration(
+                              hintText: 'Enter a todo title',
+                              suffixIcon: IconButton(
+                                onPressed: clearTextField,
+                                icon: Icon(Icons.clear),
+                              ),
+                            ),
+                            textAlign: TextAlign.start,
+                            onChanged: (newText) {
+                              _newTaskTitle = newText;
+                            },
+                            onSubmitted: (newText) {
+                              if (_newTaskTitle.isEmpty) {
+                                _newTaskTitle = 'No Title';
+                              }
+                              taskList.addTask(_newTaskTitle);
+                              clearTextField();
+                            },
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Text(
+                                '${watch(isNotDoneTasksCount)} tasks left',
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                InkWell(
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: Text('All'),
+                                  ),
+                                  onTap: () => filter.state = Filter.all,
+                                ),
+                                InkWell(
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: Text('Active'),
+                                  ),
+                                  onTap: () => filter.state = Filter.active,
+                                ),
+                                InkWell(
+                                  onTap: () => filter.state = Filter.done,
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: Text('Done'),
+                                  ),
+                                ),
+                                InkWell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Text(
+                                      'Delete Done',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  onTap: () {
+                                    final doneTasks = allTasks
+                                      .where((task) => task.isDone)
+                                      .toList();
+                                    if (doneTasks.isNotEmpty) {
+                                      taskList.deleteDoneTasks();
+                                      showSnackBar(
+                                        previousTasks: allTasks,
+                                        taskList: taskList,
+                                        content:
+                                        'Done tasks have been deleted.',
+                                        scaffoldState: Scaffold.of(context),
+                                      );
+                                    }
+                                  },
+                                ),
+                                InkWell(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Text(
+                                      'Delete All',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  onTap: () {
+                                    if (allTasks.isNotEmpty) {
+                                      taskList.deleteAllTasks();
+                                      showSnackBar(
+                                        previousTasks: allTasks,
+                                        taskList: taskList,
+                                        content: 'All tasks have been deleted.',
+                                        scaffoldState: Scaffold.of(context),
+                                      );
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        final task = displayedTasks[index];
+                        return TaskTile(
+                          taskTitle: task.title,
+                          isChecked: task.isDone,
+                          checkboxCallback: (bool value) {
+                            taskList.toggleDone(task.id);
+                          },
+                          longPressCallback: () {
+                            taskList.deleteTask(task);
+                            showSnackBar(
+                              previousTasks: displayedTasks,
+                              taskList: taskList,
+                              content: '${task.title} has been deleted.',
+                              scaffoldState: Scaffold.of(context),
+                            );
+                          },
+                        );
+                      },
+                      itemCount: displayedTasks.length,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
