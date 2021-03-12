0d0b749c15f48f2ed10e9abec245baaa29ecf133
杉岡成哉 <sugiokaseiya@team-lab.com>
2021-03-12 12:02:30 +0900

    firebaseの初期化処理導入

---
+++ lib/screen/firebase_launch_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:moor_sample/screen/home_screen.dart';
+import 'package:moor_sample/screen/launch_screen.dart';
+
+class FirebaseLaunchScreen extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return HomeScreen();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return LaunchScreen();
+      },
+    );
+  }
+}
---
+++ lib/screen/launch_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LaunchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
