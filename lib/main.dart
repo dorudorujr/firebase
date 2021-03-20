ef57f6e9b0255aec164d9a3bcbbebc1b69ca882b
杉岡成哉 <sugiokaseiya@team-lab.com>
2021-03-20 23:54:07 +0900

    firebaseの初期化方法を修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-import 'package:moor_sample/screen/firebase_launch_screen.dart';
+import 'package:moor_sample/screen/launch_screen.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ProviderScope(
-      child: FirebaseLaunchScreen(),
+      child: LaunchScreen(),
     ),
   );
 }
