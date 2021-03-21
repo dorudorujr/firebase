c452a6584e200741ec8e3700b0232160e7b051a2
杉岡成哉 <sugiokaseiya@team-lab.com>
2021-03-21 21:46:38 +0900

    ログイン状態を確認して出し分けるよう修正

--- lib/screen/launch_screen.dart
+++ lib/screen/launch_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:moor_sample/screen/signin_screen.dart';
+import 'package:moor_sample/screen/home_screen.dart';
 
 class LaunchScreen extends StatelessWidget {
+
+  final FirebaseAuth fireBaseAuth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignInScreen(),
+      home: fireBaseAuth.currentUser != null ? HomeScreen() : SignInScreen(),
     );
   }
 }
