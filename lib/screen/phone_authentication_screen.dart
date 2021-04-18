0661987a1846cdaf7e39ea4e345635c543e48461
杉岡成哉 <sugiokaseiya@team-lab.com>
2021-04-18 22:53:28 +0900

    consumerを追加

--- lib/screen/phone_authentication_screen.dart
+++ lib/screen/phone_authentication_screen.dart
@@ -13,45 +13,48 @@ class PhoneAuthenticationScreen extends StatelessWidget {
     return Scaffold(
       key: _scaffoldKey,
       resizeToAvoidBottomPadding: false,
-      body: Padding(padding: const EdgeInsets.all(8),
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                controller: _phoneNumberController,
-                decoration: const InputDecoration(labelText: 'Phone number (+xx xxx-xxx-xxxx)'),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                alignment: Alignment.center,
-                child: RaisedButton(
-                  color: Colors.greenAccent[400],
-                  child: Text("認証コード要求"),
-                  onPressed: () async {
-                    //verifyPhoneNumber();
-                  },
+      body: Consumer(
+      builder: (context, watch, child) {
+        return Padding(padding: const EdgeInsets.all(8),
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  controller: _phoneNumberController,
+                  decoration: const InputDecoration(labelText: 'Phone number (+xx xxx-xxx-xxxx)'),
                 ),
-              ),
-              TextFormField(
-                controller: _smsController,
-                decoration: const InputDecoration(labelText: 'Verification code'),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 16.0),
-                alignment: Alignment.center,
-                child: RaisedButton(
-                  color: Colors.greenAccent[200],
-                  onPressed: () async {
-                    //signInWithPhoneNumber();
-                  },
-                  child: Text("Sign in")),
-              ),
-            ],
-          )
-        ),
-      )
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  alignment: Alignment.center,
+                  child: RaisedButton(
+                    color: Colors.greenAccent[400],
+                    child: Text("認証コード要求"),
+                    onPressed: () async {
+                      //verifyPhoneNumber();
+                    },
+                  ),
+                ),
+                TextFormField(
+                  controller: _smsController,
+                  decoration: const InputDecoration(labelText: 'Verification code'),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  alignment: Alignment.center,
+                  child: RaisedButton(
+                    color: Colors.greenAccent[200],
+                    onPressed: () async {
+                      //signInWithPhoneNumber();
+                    },
+                    child: Text("Sign in")),
+                ),
+              ],
+            )
+          ),
+        );
+      })
     );
   }
 
